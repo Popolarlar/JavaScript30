2a272dd7d6f0fccb35bfe25d2957e6fbda7e6671
jin <s3736719@student.rmit.edu.au>
2020-07-16 16:40:02 +1000

    Day11: Correct

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,51 +1,78 @@
 const video = document.querySelector("video");
-const playButtons = document.querySelectorAll(".player__button");
+const playButton = document.querySelector(".toggle");
+const skipButtons = document.querySelectorAll("[data-skip]");
+const progress = document.querySelector(".progress");
+const progressBar = progress.querySelector(".progress__filled");
+const sliders = document.querySelectorAll("input[type = range]");
+const fullscreenButton = document.querySelector(".fullscreen");
 
-addEventListener("keyup", playPause);
-
-playButtons.forEach((playButton) => {
-  if (playButton.classList.contains("toggle")) {
-    playButton.addEventListener("click", playPause);
-  } else {
-    playButton.addEventListener("click", playSkip);
-  }
+// addEventListener("keyup", playPause);
+video.addEventListener("click", playPause);
+video.addEventListener("play", updateButton);
+video.addEventListener("pause", updateButton);
+video.addEventListener("timeupdate", updateProgressBar);
+playButton.addEventListener("click", playPause);
+sliders.forEach((slider) => {
+  slider.addEventListener("change", updateRange);
+  slider.addEventListener("input", updateRange);
 });
+skipButtons.forEach((skipButton) => {
+  skipButton.addEventListener("click", playSkip);
+});
+progress.addEventListener("click", scrub);
+let isDragging = false;
+progress.addEventListener("mousedown", () => {
+  isDragging == true;
+});
+progress.addEventListener("mouseup", () => {
+  isDragging == false;
+});
+progress.addEventListener("mousemove", (e) => isDragging && scrub(e));
+fullscreenButton.addEventListener("click", handleFullscreen);
 
 function playPause(e) {
-  if (e.type === "keyup" && e.keycode != 33) {
-    return;
-  }
+  // if (e.type === "keyup" && e.keycode != 33) {
+  //   return;
+  // }
   video.paused ? video.play() : video.pause();
 }
 
+function updateButton() {
+  playButton.textContent = this.paused ? "❚ ❚" : "►";
+}
+
+function updateProgressBar() {
+  const percentage = (this.currentTime / this.duration) * 100;
+  progressBar.style.flexBasis = percentage + "%";
+}
+
 function playSkip(e) {
-  video.pause();
-  video.currentTime = video.currentTime + parseFloat(e.target.dataset.skip);
-  video.play();
+  // video.pause();
+  video.currentTime += parseFloat(e.target.dataset.skip);
+  // video.play();
 }
 
-const videoProgress = document.querySelector(".progress__filled");
-
-video.addEventListener("timeupdate", () => {
-  var percentage =
-    ((video.currentTime / video.duration) * 100).toFixed(0) + "%";
-  videoProgress.style.flexBasis = percentage;
-});
-
-console.log(video.currentTime);
-
-const volumeSlider = document.querySelector("input[name=volume]");
-volumeSlider.addEventListener("change", changeVolume);
-volumeSlider.addEventListener("input", changeVolume);
-
-function changeVolume(e) {
-  video.volume = parseFloat(e.target.value);
+function updateRange() {
+  video[this.name] = this.value;
 }
 
-const speedSlider = document.querySelector("input[name=playbackRate]");
-speedSlider.addEventListener("change", changeSpeed);
-speedSlider.addEventListener("input", changeSpeed);
-
-function changeSpeed(e) {
-  video.playbackRate = parseFloat(e.target.value);
+function scrub(e) {
+  console.log("dragging");
+  const scrubTime = video.duration * (e.offsetX / this.offsetWidth);
+  video.currentTime = scrubTime;
+}
+function handleFullscreen() {
+  console.log("fullscreen");
+  if (video.requestFullscreen) {
+    video.requestFullscreen();
+  } else if (video.mozRequestFullScreen) {
+    /* Firefox */
+    video.mozRequestFullScreen();
+  } else if (video.webkitRequestFullscreen) {
+    /* Chrome, Safari and Opera */
+    video.webkitRequestFullscreen();
+  } else if (video.msRequestFullscreen) {
+    /* IE/Edge */
+    video.msRequestFullscreen();
+  }
 }
